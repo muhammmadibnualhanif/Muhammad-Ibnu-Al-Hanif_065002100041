8c9d7edbebfa2c18a6d52c07902507d48cf3f24b
muhammmadibnualhanif <91421315+muhammmadibnualhanif@users.noreply.github.com>
2021-09-26 21:31:10 +0700

    Add files via upload

---
+++ PrakAlgo Muhammad Ibnu Al Hanif 065002100041.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = ("@  @    @    @   @   @   @@@")
+b = ("@  @   @ @   @@  @   @   @  ")
+c = ("@@@@  @@@@@  @ @ @   @   @@ ")
+d = ("@  @ @     @ @   @   @   @  ")
+print (a)
+print (b)
+print (c)
+print (d)
+
+x = 2*3
+y = 1*3
+z = x*y
+print (z)
+
+#huruf kecil ke kapital
+string = "algo itu asik"
+print
+cara= string.upper()
+print(cara)
